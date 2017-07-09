d9e1eaf076894b8aa2a585e42286cf69daa58590
Jared Goodwin <translucency@outlook.com>
2017-07-09 08:35:06 -0700

    Added nuspec.

--- WPF_Auto_Updater/Properties/AssemblyInfo.cs
+++ WPF_Auto_Updater/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AutoUpdater")]
+[assembly: AssemblyTitle("WPF Auto Update")]
 [assembly: AssemblyDescription("Adds automatic updating to WPF applications.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Translucency")]
-[assembly: AssemblyProduct("WPF Auto Updater")]
+[assembly: AssemblyProduct("WPF Auto Update")]
 [assembly: AssemblyCopyright("Copyright ©  2017 Translucency")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
